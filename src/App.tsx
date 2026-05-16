4500e8fec0178e29eadcbc776c407a99c5e3a6fd
kasanvith <kasanvithsoluctions@gmail.com>
2026-05-16 18:19:06 -0300

    Refactor App component by removing unused imports
    Removed unused imports and cleaned up the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,3 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Header } from './components/Header'
-import { HeroFrase } from './components/HeroFrase'
-import { HeroImage } from './components/HeroImage'
-import { HeroForm } from './components/HeroForm'
-import { Nav } from './components/Nav'
-import { SearchBar } from './components/SearchBar'
-import { HeaderNav } from './components/HeaderNav'
-import { FaSearch } from 'react-icons/fa'
-
-
 
 
 
@@ -19,12 +6,6 @@ function App(){
 return (
   
 
-  <main className="min-h-screen bg-stone-200">
-    <HeaderNav/>
-    <HeroFrase/>
-    <HeroImage/>
-    <HeroForm/>
-    </main>
     
 
  
